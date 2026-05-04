60fc4445cb05c83713b125c649fd2d0b2cdbb358
Muhammad Reza Pahlevi <rezaworku31@gmail.com>
2026-05-04 16:42:19 +0700

    refactor : adding response utils and base for api response

--- src/main/java/com/example/transactionmanagement/controller/TransactionController.java
+++ src/main/java/com/example/transactionmanagement/controller/TransactionController.java
@@ -4,11 +4,14 @@ import com.example.transactionmanagement.dto.CreateTransactionPayload;
 import com.example.transactionmanagement.dto.PaginationResponse;
 import com.example.transactionmanagement.entity.TransactionEntity;
 import com.example.transactionmanagement.service.TransactionServiceInterface;
+import com.example.transactionmanagement.utils.BaseResponse;
+import com.example.transactionmanagement.utils.ResponseUtils;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,13 +28,15 @@ public class TransactionController {
     }
 
     @GetMapping("/transactions/{reference}")
-    public TransactionEntity getOneTransaction(@PathVariable String reference) {
-        return transactionService.getOneTransaction(reference);
+    public ResponseEntity<BaseResponse<TransactionEntity>> getOneTransaction(@PathVariable String reference) {
+        return ResponseUtils.success(transactionService.getOneTransaction(reference), "Sucessfully get one data");
+//        return transactionService.getOneTransaction(reference);
     }
 
     @PostMapping("/transactions")
-    public TransactionEntity createTransaction(@Valid @RequestBody CreateTransactionPayload payload) {
-        return transactionService.createTransaction(payload);
+    public ResponseEntity<BaseResponse<TransactionEntity>> createTransaction(@Valid @RequestBody CreateTransactionPayload payload) {
+        return ResponseUtils.success(transactionService.createTransaction(payload),"Succesfully created data");
+        //        return transactionService.createTransaction(payload);
     }
 
     @PutMapping("/transactions/{id}")
